5e7860a2b39b3b748480c644b2d1ae3fca786494
patient-creator <25027201@mvula.univen.ac.za>
2025-04-04 13:21:48 +0200

    Add files via upload

---
+++ BurmaShaving.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.burmashave;
+
+/**
+ *
+ * @author 25027201
+ */
+import javax.swing.JOptionPane;
+public class BurmaShaving {
+
+    public static void main(String[] args) {
+       
+            
+         JOptionPane.showMessageDialog(null," Shaving brushes");
+            JOptionPane.showMessageDialog(null," you'll soon see 'em ");
+         JOptionPane.showMessageDialog(null, " On a shelf ");
+           JOptionPane.showMessageDialog(null" In some museum ");
+            JOptionPane.showMessageDialog(null, " Burma shave ");
+        
+     
+        }
+}
